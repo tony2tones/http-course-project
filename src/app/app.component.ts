8d689b20874b0b915f473072ff1e559bbe777f20
Anthony <anthony.marque@gmail.com>
2019-04-29 16:06:30 +0200

    added firebase signup config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  loadedFeature = 'recipe';
+export class AppComponent implements OnInit {
+  loadedFeature = "recipe";
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCcInDlBNMagcYyYILXBgRsoivrmrzppbo",
+      authDomain: "recipe-4f1fb.firebaseapp.com"
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
